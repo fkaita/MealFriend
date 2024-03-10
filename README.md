6c923cd83dfdd254d90cd1a48454ab0919e26a36
fkaita <spkaikai@gmail.com>
2024-03-10 15:27:43 +0900

    set basic timer function

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # mealfriend
 
-A new Flutter project.
+This is a flutter app to help user to eat slower and less lonely.
+
+## Problem
+
+I personally eat too quick (like 5 min for lunch and 10 min for dinner!), and I wanted to change this habit. I heard eating slow is good for health and staying in shape too. (Need citations)
+
+Moreover, I'm single and often eat alone. It will be nice if I have an app to nudge me to eat slower and make me feel less lonelier.
+
+## Concept
+
+A meal friend to eat with you. He/she will nudge you to eat slower and make you less lonely.
+
+## Functions
+
+Help user to eat slower anc check the record.
+
+1. Visually nudge user to eat slower. (The meal friend will eat together with you, and you'll keep the same pace with him/her.)
+
+2. Record time spent on the meal, and you can check the information later.
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,104 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TimerPage(),
+    );
+  }
+}
+
+class TimerPage extends StatefulWidget {
+  @override
+  _TimerPageState createState() => _TimerPageState();
+}
+
+enum TimerState { initial, started, paused }
+
+class _TimerPageState extends State<TimerPage> {
+  int seconds = 0;
+  final int maxSeconds = 600;
+  TimerState timerState = TimerState.initial;
+  Timer? timer;
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      setState(() {
+        seconds++;
+      });
+    });
+    setState(() {
+      timerState = TimerState.started;
+    });
+  }
+
+  void pauseTimer() {
+    timer?.cancel();
+    setState(() {
+      timerState = TimerState.paused;
+    });
+  }
+
+  void finishTimer() {
+    timer?.cancel();
+    setState(() {
+      seconds = 0;
+      timerState = TimerState.initial;
+      // Go to next stage here
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Expanded(child: Placeholder()),
+            LinearProgressIndicator(
+              value: seconds / maxSeconds,
+              minHeight: 12,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              '$seconds',
+              style: TextStyle(fontSize: 24),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              ElevatedButton(
+                  onPressed: () {
+                    if (timerState == TimerState.started) {
+                      pauseTimer();
+                    } else {
+                      startTimer();
+                    }
+                  },
+                  child: Text(timerState == TimerState.started
+                      ? 'Pause'
+                      : timerState == TimerState.paused
+                          ? 'Restart'
+                          : 'Start')),
+              if (timerState != TimerState.initial)
+                ElevatedButton(
+                    onPressed: () {
+                      finishTimer();
+                    },
+                    child: Text('Finish'))
+            ]),
+            SizedBox(
+              height: 48,
+            )
+          ],
         ),
       ),
     );
